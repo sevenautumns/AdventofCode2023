da889d07620778b9391d1d7f13188f00f15e4169
Sven Friedrich <sven@autumnal.de>
2023-12-06 14:15:54 +0100

    refactor: organize days

--- flake.nix
+++ flake.nix
@@ -15,10 +15,11 @@
             inherit system;
             overlays = [ devshell.overlays.default ];
           };
+          basename = x: builtins.head (lib.strings.splitString "-" x);
           capital = text: (lib.strings.toUpper (builtins.substring 0 1 text) + builtins.substring 1 100 text);
           builder = name: pkgs.runCommand name { } ''
             mkdir --parent $out/bin
-            ${pkgs.haskellPackages.ghc}/bin/ghc -O2 -odir /build -hidir /build -o $out/bin/${name} ${self}/${name}.hs
+            ${pkgs.haskellPackages.ghc}/bin/ghc -O2 -odir /build -hidir /build -o $out/bin/${name} ${self}/${basename name}/${name}.hs
           '';
           package_names = (map (x: "day" + (toString x)) (lib.range 1 days)) ++ (map (x: "day" + (toString x) + "-part2") (lib.range 1 days));
           days = 6;
@@ -35,29 +36,41 @@
               ormolu
               cocogitto
             ];
+            git.hooks = {
+              enable = true;
+              pre-commit.text = "nix flake check";
+            };
             commands = (builtins.map
-              (name: {
-                name = "run-${name}";
-                command = "nix run .#${name}";
-                help = "Run ${capital name} code";
-              })
+              (name:
+                {
+                  name = "run-${name}";
+                  command = ''
+                    mkdir --parent $PRJ_ROOT/${basename name}
+                    cd $PRJ_ROOT/${basename name}
+                    nix run .#${name}
+                  '';
+                  help = "Run ${capital name} code";
+                })
               package_names)
             ++ (builtins.map
-              (name: {
-                name = "watch-${name}";
-                command = ''
-                  mkdir --parent $PRJ_ROOT/build
-                  ${pkgs.cargo-watch}/bin/cargo-watch watch \
-                    --workdir $PRJ_ROOT \
-                    --shell "${pkgs.ghc}/bin/ghc -O2 -odir $PRJ_ROOT/build -hidir $PRJ_ROOT/build -o $PRJ_ROOT/build/${name} $PRJ_ROOT/${name}.hs && $PRJ_ROOT/build/${name}"
-                '';
-                help = "Watch ${capital name}";
-                category = "dev";
-              })
+              (name:
+                {
+                  name = "watch-${name}";
+                  command = ''
+                    mkdir --parent $PRJ_ROOT/build
+                    mkdir --parent $PRJ_ROOT/${basename name}
+                    ${pkgs.cargo-watch}/bin/cargo-watch watch \
+                      --workdir $PRJ_ROOT/${basename name} \
+                      --shell "${pkgs.ghc}/bin/ghc -O2 -odir $PRJ_ROOT/build -hidir $PRJ_ROOT/build -o $PRJ_ROOT/build/${name} $PRJ_ROOT/${basename name}/${name}.hs && $PRJ_ROOT/build/${name}"
+                  '';
+                  help = "Watch ${capital name}";
+                  category = "dev";
+                })
               package_names) ++ [{
               name = "format-haskell";
               command = ''
-                ${pkgs.ormolu}/bin/ormolu --mode inplace *.hs
+                cd $PRJ_ROOT
+                ${pkgs.ormolu}/bin/ormolu --mode inplace */*.hs
               '';
               help = "format all haskell files";
               category = "dev";
@@ -68,9 +81,13 @@
           # always check these
           checks = {
             nixpkgs-fmt = pkgs.runCommand "nixpkgs-fmt"
+              { } "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check ${self}; touch $out";
+            ormolu = pkgs.runCommand "ormolu"
+              { } "${pkgs.ormolu}/bin/ormolu --mode check ${self}/*/*.hs; touch $out";
+            build-all = pkgs.runCommand "build-all"
               {
-                nativeBuildInputs = [ pkgs.nixpkgs-fmt ];
-              } "nixpkgs-fmt --check ${./.}; touch $out";
+                nativeBuildInputs = map (x: self.packages.${system}.${x}) package_names;
+              } "touch $out";
           };
 
           # instructions for the CI server
