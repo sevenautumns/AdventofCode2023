57ce28bb64054904c114067679a28fc78351cb6d
Sven Friedrich <sven@autumnal.de>
2023-12-06 12:06:03 +0100

    feat: day6

---
+++ day6-part2.hs
@@ -0,0 +1,33 @@
+import Data.Char (isDigit)
+
+--                   time   dist   press
+findShortestPress :: Int -> Int -> Int
+findShortestPress = findPressHelper 0 1
+
+--                  time   dist   press
+findLongestPress :: Int -> Int -> Int
+findLongestPress time = findPressHelper time (-1) time
+
+findPressHelper :: Int -> Int -> Int -> Int -> Int
+findPressHelper press delta time distance
+  | dist > distance = press
+  | otherwise = findPressHelper (press + delta) delta time distance
+  where
+    dist = (time - press) * press
+
+getWins :: Int -> Int -> Int
+getWins time distance = max - min + 1
+  where
+    min = findShortestPress time distance
+    max = findLongestPress time distance
+
+calculateMargin :: [Int] -> Int
+calculateMargin (x : y : _) = getWins x y
+
+parseNumbers :: String -> Int
+parseNumbers input = read $ filter isDigit input
+
+main :: IO ()
+main = do
+  input <- readFile "day6-input"
+  print $ calculateMargin $ map parseNumbers (lines input)
---
+++ day6.hs
@@ -0,0 +1,45 @@
+import Data.Char (isDigit)
+
+--                   time   dist   press
+findShortestPress :: Int -> Int -> Int
+findShortestPress = findPressHelper 0 1
+
+--                  time   dist   press
+findLongestPress :: Int -> Int -> Int
+findLongestPress time = findPressHelper time (-1) time
+
+findPressHelper :: Int -> Int -> Int -> Int -> Int
+findPressHelper press delta time distance
+  | dist > distance = press
+  | otherwise = findPressHelper (press + delta) delta time distance
+  where
+    dist = (time - press) * press
+
+getWins :: Int -> Int -> Int
+getWins time distance = max - min + 1
+  where
+    min = findShortestPress time distance
+    max = findLongestPress time distance
+
+countWins :: [Int] -> [Int] -> Int
+countWins [] [] = 1
+countWins (t : ts) (d : dx) = getWins t d * countWins ts dx
+
+calculateMargin :: [[Int]] -> Int
+calculateMargin (x : y : _) = countWins x y
+
+parseNumbers :: String -> [Int]
+parseNumbers input = parseNumbersHelper input ""
+
+parseNumbersHelper :: String -> String -> [Int]
+parseNumbersHelper [] [] = []
+parseNumbersHelper [] x = [read x]
+parseNumbersHelper (x : xs) y
+  | isDigit x = parseNumbersHelper xs (y ++ [x])
+  | not (null y) = read y : parseNumbersHelper xs ""
+  | otherwise = parseNumbersHelper xs ""
+
+main :: IO ()
+main = do
+  input <- readFile "day6-input"
+  print $ calculateMargin $ map parseNumbers (lines input)
