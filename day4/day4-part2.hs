a954549ea75273e709b0696db1df227e550d33f2
Sven Friedrich <sven@autumnal.de>
2023-12-07 21:13:42 +0100

    refactor: improve day4

--- day4/day4-part2.hs
+++ day4/day4-part2.hs
@@ -5,41 +5,31 @@ splitOnFirst :: Char -> String -> (String, String)
 splitOnFirst sep str = let (a, b) = break (== sep) str in (a, drop 1 b)
 
 intersect :: [Int] -> [Int] -> [Int]
-intersect [] _ = []
-intersect (x : xs) compare
-  | x `elem` compare = x : rest
-  | otherwise = rest
-  where
-    rest = intersect xs compare
+intersect left right = [x | x <- left, x `elem` right]
 
 filterNumbers :: String -> [Int]
-filterNumbers input = map read (filter (not . isSpace . head) (groupBy (\a b -> isDigit a && isDigit b) input))
+filterNumbers = map read . words
 
 solveGame :: String -> Int
 solveGame input = do
-  let (_, rest) = splitOnFirst ':' input
-  let (winning, have) = splitOnFirst '|' rest
-  let filtered_winning = filterNumbers winning
-  let filtered_have = filterNumbers have
-  let intersection = filtered_winning `intersect` filtered_have
+  let numbers = snd $ splitOnFirst ':' input
+  let (winning, have) = splitOnFirst '|' numbers
+  let intersection = filterNumbers winning `intersect` filterNumbers have
   length intersection
 
 incCounts :: Int -> Int -> [Int] -> [Int]
 incCounts 0 _ rest = rest
-incCounts numbersToIncrease increaseBy (y : ys) = do
-  (y + increaseBy) : incCounts (numbersToIncrease - 1) increaseBy ys
+incCounts numbersToIncrease increaseBy (y : ys) = (y + increaseBy) : incCounts (numbersToIncrease - 1) increaseBy ys
 
-walkGames :: [String] -> [Int] -> Int
-walkGames [] [] = 0
-walkGames (x : xs) (y : ys) = do
+walkOverGames :: [String] -> [Int] -> Int
+walkOverGames [] [] = 0
+walkOverGames (x : xs) (y : ys) = do
   let gain = solveGame x
   let counts = incCounts gain y ys
-  walkGames xs counts + y
+  walkOverGames xs counts + y
 
 solveGames :: [String] -> Int
-solveGames input = do
-  let counts = replicate (length input) 1
-  walkGames input counts
+solveGames input = walkOverGames input (replicate (length input) 1)
 
 main :: IO ()
 main = readFile "day4-input" >>= print . solveGames . lines
--- day4/day4.hs
+++ day4/day4.hs
@@ -5,27 +5,24 @@ splitOnFirst :: Char -> String -> (String, String)
 splitOnFirst sep str = let (a, b) = break (== sep) str in (a, drop 1 b)
 
 intersect :: [Int] -> [Int] -> [Int]
-intersect [] _ = []
-intersect (x : xs) compare
-  | x `elem` compare = x : rest
-  | otherwise = rest
-  where
-    rest = intersect xs compare
+intersect left right = [x | x <- left, x `elem` right]
 
 filterNumbers :: String -> [Int]
-filterNumbers input = map read (filter (not . isSpace . head) (groupBy (\a b -> isDigit a && isDigit b) input))
+filterNumbers = map read . words
+
+calcPayout :: [a] -> Int
+calcPayout [] = 0
+calcPayout x = 2 ^ (length x - 1)
 
 solveGame :: String -> Int
 solveGame input = do
-  let (_, rest) = splitOnFirst ':' input
-  let (winning, have) = splitOnFirst '|' rest
-  let filtered_winning = filterNumbers winning
-  let filtered_have = filterNumbers have
-  let intersection = filtered_winning `intersect` filtered_have
-  if null intersection then 0 else 2 ^ (length intersection - 1)
+  let numbers = snd $ splitOnFirst ':' input
+  let (winning, have) = splitOnFirst '|' numbers
+  let intersection = filterNumbers winning `intersect` filterNumbers have
+  calcPayout intersection
 
 solveGames :: [String] -> Int
-solveGames input = sum $ map solveGame input
+solveGames = sum . map solveGame
 
 main :: IO ()
 main = readFile "day4-input" >>= print . solveGames . lines
